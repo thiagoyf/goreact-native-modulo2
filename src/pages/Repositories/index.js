95c6a3a0d4f83511e31766ba556b4320e562e8ca
Thiago Yudi Fukunaga <thiagoyudifukunaga@gmail.com>
2019-06-12 10:49:37 -0400

    Styling the list

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import api from '~/services/api';
 import AsyncStorage from '@react-native-community/async-storage';
 
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, FlatList } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
+
+import RepositoryItem from './RepositoryItem';
 import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
@@ -24,22 +26,43 @@ export default class Repositories extends Component {
   state = {
     data: [],
     loading: true,
+    refreshing: false,
   };
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
     const username = await AsyncStorage.getItem('@Githuber:username');
     const { data } = await api.get(`/users/${username}/repos`);
 
-    this.setState({ data, loading: false });
-  }
+    this.setState({ data, loading: false, refreshing: false });
+  };
 
-  renderList = () => <Text>Lista</Text>;
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadRepositories}
+        refreshing={refreshing}
+      />
+    );
+  };
 
   render() {
     const { loading } = this.state;
 
     return (
-      <View>
+      <View style={styles.container}>
         <Header title="Repositórios" />
         {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
       </View>
