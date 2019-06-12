84b7f2616c410a06b484a50a689ba5a47af9192d
Thiago Yudi Fukunaga <thiagoyudifukunaga@gmail.com>
2019-06-12 09:56:16 -0400

    Adding screen Organizations and putting Repositories and Organizations in tabs

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
 
-const Organizations = () => <View />;
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Header from '~/components/Header';
+
+const Organizations = () => (
+  <View>
+    <Header title="Organizações" />
+  </View>
+);
+
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
 
@@ -9,5 +12,15 @@ const Repositories = () => (
     <Header title="Repositórios" />
   </View>
 );
+
+const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Repositories.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Repositories;
